e281edc871f81a86c7569fe2fbe3ebbf1a31c2f2
Rich Stadnick <rich.stadnick@gmail.com>
2021-08-15 16:27:50 -0400

    Remove first attempt

--- old/scripts/db/1_create_db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Create the db if first run and not exists
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE tfg;
-EOSQL
